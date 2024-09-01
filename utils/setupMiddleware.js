c6aa0e2c85fe2eafb80da7e9f27f44df4fa1bc48
Haluk Isik <isikhaluk@gmail.com>
2024-09-01 22:59:39 +0300

    removed preflight requests

--- utils/setupMiddleware.js
+++ utils/setupMiddleware.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import corsMiddleware from './cors.js';
 
 
 const setupMiddleware = (app) => {
     // CORS Middleware
     app.use(corsMiddleware);
-    app.options('*', cors()); // Respond to preflight requests
+    // app.options('*', cors()); // Respond to preflight requests
 
     // Middleware to parse JSON bodies
     app.use(express.json());
